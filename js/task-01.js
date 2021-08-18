70c99dc13874418afd5262415283b9ca4d45fd2c
Dmitry Dovgopol <dovgopol.ds@gmail.com>
2021-08-18 17:55:01 +0300

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,29 @@
+const mainListRef = document.querySelector('#categories');
+// console.log(mainListRef);
+
+const liItemRef = mainListRef.querySelectorAll('li.item');
+
+//работает до цифры '20' включительно ;)
+const msg = arg => {
+    if (arg.length === 1) {
+        return `В списке 1 категория.`;
+    };
+
+    if (arg.length >= 2 && arg.length < 5) {
+        return `В списке ${arg.length} категории.`;
+    };
+
+    return `В списке ${arg.length} категорий.`;
+}
+
+console.log(msg(liItemRef));
+
+///////////////////////////////////////////////////////
+
+console.log(liItemRef)
+
+liItemRef.forEach(item => {
+    const titleRef = item.querySelector('h2').textContent;
+    const countRef = item.querySelectorAll('li').length;
+    console.log(`Категория: ${titleRef}, Количество элементов: ${countRef}`)
+});
